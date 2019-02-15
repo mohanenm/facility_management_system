d94b6c115aa62976de5cdaec6f0e300f4136b7fe
Ethan Mohan <ethannmohan@gmail.com>
2019-02-15 14:18:01 -0600

    maintain

--- src/main/java/base_layer/Facility.java
+++ src/main/java/base_layer/Facility.java
@@ -1,6 +1,3 @@
-
-
-
 package base_layer;
 
 public class Facility {
---
+++ src/main/java/maintain/Maintenance.java
@@ -0,0 +1,28 @@
+package maintain;
+
+
+import base_layer.Facility;
+
+public class Maintenance extends Facility{
+
+    private String details;
+    private int cost;
+
+    public Maintenance(){}
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+
+    public void setCost(int cost){
+        this.cost = cost;
+    }
+}
