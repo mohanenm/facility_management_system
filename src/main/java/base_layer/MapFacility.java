ab204e96acb426a9d02ce8eb8b9d182af49690bb
Ethan <theejdavidson@gmail.com>
2019-02-18 15:04:17 -0600

    added address, floor, room classes

---
+++ src/main/java/base_layer/MapFacility.java
@@ -0,0 +1,12 @@
+package base_layer;
+
+import java.util.ArrayList;
+
+public class MapFacility {
+    ArrayList<Room> facilityRoomList = new ArrayList<Room>();
+
+    public void addRooms(ArrayList facilityRoomList){
+        facilityRoomList.add();
+    }
+
+}
